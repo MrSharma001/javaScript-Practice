d18f3fcb5ffccf4a6e0a5d8a5f9711ee09801f42
MrSharma001 <110118837+MrSharma001@users.noreply.github.com>
2023-12-21 08:03:26 +0000

    variable

---
+++ 01_bsic/about_variable.js
@@ -0,0 +1,12 @@
+const userId = 12345;
+let userName = "gaurav";
+var city = "delhi";
+PNumber = "1234567890";
+
+// userId = 43;
+userName = "rahul";
+city = "up";
+PNumber = "6543234567"
+
+console.log(userId);
+console.table([userId, userName, city, PNumber]);
